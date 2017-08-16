86530e7de1d409f191b0e32e0f7e983ac737236f
Erinn Springer <erinnlspringer@gmail.com>
2017-08-16 10:19:12 +0200

    changes from class

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :stars, inclusion: { in: [1,2,3], allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+
+    member do
+      get 'chef', to: "restaurants#chef"
+    end
+
+    resources :reviews, only: [:new, :create]
+
+    collection do
+      get 'top', to: "restaurants#top"
+    end
+
+
+
+  end
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
